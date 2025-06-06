b67083af56339e3f70a97237deb9ad984c7a4fe6
apesoda <43443669+apesoda@users.noreply.github.com>
2025-06-06 21:52:30 +0200

    added second indicator but this really is a terriblw way to make this, need to sort something better out

--- active.sh
+++ active.sh
@@ -6,16 +6,29 @@ while true; do
 		continue
 	fi
 
-	x=$(echo "$info" | jq '.at[0]')
-	y=$(echo "$info" | jq '.at[1]')
+	x_t=$(echo "$info" | jq '.at[0]')
+	y_t=$(echo "$info" | jq '.at[1]')
+	
+	offset_x_b=$( echo "$info" | jq '.size[0]')
+	offset_y_b=$( echo "$info" | jq '.size[1]')
 
-	if [ -z "$x" ] || [ -z "$y" ]; then
+	x_b=$(( $x_t + $offset_x_b ))
+	y_b=$(( $y_t + $offset_y_b ))
+
+	if [ -z "$x_t" ] || [ -z "$y_t" ]; then
 		continue
 	fi
 
-	new_x=$(( $x - 10 ))
-	new_y=$(( $y - 55 ))
+	if [ -z "$x_b" ] || [ -z "$y_b" ]; then
+		continue
+	fi
+	new_x_t=$(( $x_t - 10 ))
+	new_y_t=$(( $y_t - 55 ))
 
-	eww open active-indicator --arg pos_x=$new_x --arg pos_y=$new_y
+	new_x_b=$(( $x_b - 10 ))
+	new_y_b=$(( $y_b - 55 ))
+	
+	eww open active-indicator-t --arg pos_x=$new_x_t --arg pos_y=$new_y_t
+	eww open active-indicator-b --arg pos_x=$new_x_b --arg pos_y=$new_y_b
 	#sleep 0.1
 done
